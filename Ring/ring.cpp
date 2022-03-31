186173d7c59146da1ceca27f95959c425b8ff32b
abambhaniya3 <abambhaniya3@gatech.edu>
2022-04-01 01:08:04 +0530

    changes from discussion

--- Ring/ring.cpp
+++ Ring/ring.cpp
@@ -24,21 +24,22 @@ void ring(
 
     static Router node[NUM_NODES]; 
 
-    ap_unit[DATA_WIDTH] link_in[NUM_NODES]; // why not just have a array of router id. Like a adjacent map to fill out. Even we can extend to mesh: one key with 4 elements: 0 E, 1 S, 2 W, 3 N, etc
-    ap_unit[DATA_WIDTH] link_out[NUM_NODES];
+    ap_unit[DATA_WIDTH] link_east[NUM_NODES]; // why not just have a array of router id. Like a adjacent map to fill out. Even we can extend to mesh: one key with 4 elements: 0 E, 1 S, 2 W, 3 N, etc
+    ap_unit[DATA_WIDTH] link_west[NUM_NODES];
 
-    INT16 onoff_switch[NUM_NODES]; // 16 bits reserve for credit-base switch in the future
+    INT16 onoff_switch_east[NUM_NODES]; // 16 bits reserve for credit-base switch in the future
+    INT16 onoff_switch_west[NUM_NODES]; // 16 bits reserve for credit-base switch in the future
     int buffer_size = 4;
 
     node[0] = Router(0,buffer_size,routing_algorithm,traffic_pattern,NUM_NODES,num_packet_per_node
-                    link_in[0],link_out[NUM_NODES-1]); // do not understand
-    for (int i = 1 ; i < NUM_NODES; i++)
+                    link_east[0],link_west[0], link_east[NUM_NODES-1], link_west[1]); // do not understand
+    for (int i = 1 ; i < NUM_NODES -1; i++)
     {
         node[i] = Router(i,buffer_size,routing_algorithm,traffic_pattern,NUM_NODES,num_packet_per_node
-                    link_in[i],link_out[i-1]);
+                    link_east[i],link_west[i],link_east[i-1],link_west[i+1]);
     }
-//         node[NUM_NODES-1] = Router(NUM_NODES-1,buffer_size,routing_algorithm,traffic_pattern,NUM_NODES,num_packet_per_node
-//                     link_in[i],link_out[i-1]);
+        node[NUM_NODES-1] = Router(NUM_NODES-1,buffer_size,routing_algorithm,traffic_pattern,NUM_NODES,num_packet_per_node
+                    link_east[NUM_NODES-1],link_west[NUM_NODES-1],link_east[NUM_NODES-2],link_west[0]);
 
     while(total_packets_recieved < num_packet_per_node*NUM_NODES)
     {
